a851b337964c05feec963ac3adfc93c53b2d65a9
micw1417 <wu355049@gmail.com>
2024-11-03 15:51:49 -0600

    Did player interaction system and dialog system using a great yt tutorial!

--- player/dude.gd
+++ player/dude.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var speed = 100  # speed in pixels/sec
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var animated_sprite_2d: AnimatedSprite2D = $Sprite
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -14,4 +14,6 @@ func _physics_process(delta):
 		animated_sprite_2d.play("down")
 	if (direction.y < 0):
 		animated_sprite_2d.play("up")
+	if velocity == Vector2.ZERO:
+		animated_sprite_2d.stop()
 	move_and_slide()

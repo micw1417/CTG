a56b7ae74c6405c7ad2da385eb5b53ac2e0af038
micw1417 <wu355049@gmail.com>
2024-11-13 20:54:55 -0600

    hello I did some mininal work lemme try to do stuff

--- main_menu/main_menu.gd
+++ main_menu/main_menu.gd
@@ -4,7 +4,7 @@ extends Control
 
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main_startingmap/overworld.tscn")
+	get_tree().change_scene_to_file("res://game/overworld.tscn")
 
 
 func _on_quit_button_pressed() -> void:
--- npc/chicken/chicken.gd
+++ npc/chicken/chicken.gd
@@ -3,16 +3,17 @@ extends RigidBody2D
 @onready var interaction_area: InteractionArea = $InteractionArea
 @onready var sprite: AnimatedSprite2D = $Sprite
 
+
+
 var lines: Array[String] = [
-	"Congratulations",
+	"HELLO!! I AM HAPY CHICKEN"
 ]
 func _process(delta: float) -> void:
 	pass
 
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_on_interact")
-	sprite.play("idle")
+	#sprite.play("idle")
 func _on_interact():
 	DialogManager.start_dialog(global_position, lines)
-	await DialogManager.dialog_finished
-	get_tree().change_scene_to_file("res://main_startingmap/overworld.tscn")
+	

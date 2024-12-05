be71c50cbab00de40bb28904e63f107540231e37
adeenanasir50 <108042288+adeenanasir50@users.noreply.github.com>
2024-12-05 14:14:56 -0600

    FIXED THE OVEROWLRD
    it actually looks nice

--- state_manager.gd
+++ state_manager.gd
@@ -4,4 +4,4 @@ static var finishedTemple1: bool = false;
 static var finishedTemple2: bool = true;
 static var finishedTemple3: bool = false;
 static var chooeseInsult: bool = false;
-static var diedUpsidedown: bool = true;
+static var diedUpsidedown: bool = false;

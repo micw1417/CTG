c25612135ff2d07f6dd3304907d7c93cf4780713
micw1417 <wu355049@gmail.com>
2024-12-10 22:00:36 -0600

    You can talk to ares now to start fight. We need win condidtion

--- npc/npc.gd
+++ npc/npc.gd
@@ -2,28 +2,17 @@ extends RigidBody2D
 
 @onready var interaction_area: InteractionArea = $InteractionArea
 @onready var sprite: AnimatedSprite2D = $Sprite
-@onready var main : CharacterBody2D = $"../main_chrachter2"
-var speed = 50
+
 
 var lines: Array[String] = [
-	"Welcome to my temple"
-	
+	"HI EXAMPLEING"
 ]
-func _physics_process(delta): 
-	var direction = (main.position - position).normalized()
-	var move_vector = direction * speed
-	linear_velocity = move_vector
-	
-	pass
-
 func _process(delta: float) -> void:
 	pass
 
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_on_interact")
-	sprite.play("idle")
+	#sprite.play("idle")
 func _on_interact():
 	DialogManager.start_dialog(global_position, lines)
-	
-	
-	
+	await DialogManager.dialog_finished
--- state_manager.gd
+++ state_manager.gd
@@ -5,3 +5,4 @@ static var finishedTemple2: bool = false;
 static var finishedTemple3: bool = false;
 static var chooeseInsult: bool = true;
 static var diedUpsidedown: bool = false;
+static var isFightingAres: bool = false;
